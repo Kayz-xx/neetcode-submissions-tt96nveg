302640d0a66b5574472a3dac36c6335b12850b26
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2026-06-15 09:58:13 -0500

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # we are trying to validate brackets
+        # every opening bracket requires the same closing bracket
+        # we need to keep track of the last bracket
+        # now how do we make use of a stack (LIFO)
+
+        # i push all the opening brackets onto the stack
+        # pop the latest element
+        # we check for matches using a hash map
+        # check if the current parenthesis matches, if not exit
+        
+        stack = []
+        parenthesis_map = {"(": ")", "[": "]", "{": "}"}
+
+        for char in s:
+            if char in parenthesis_map.keys():
+                stack.append(char)
+            elif stack:
+                popped = stack.pop()
+                if parenthesis_map[popped] != char:
+                    return False
+            else:
+                return False
+
+        return len(stack) == 0
