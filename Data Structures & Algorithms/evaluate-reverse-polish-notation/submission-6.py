f44281c88d53721c83d3bc70ed9c50f0e3740b34
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2026-06-15 11:11:24 -0500

    Add: evaluate-reverse-polish-notation - submission-6

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-6.py
@@ -0,0 +1,38 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        '''
+        haha i learned this in high school, let's see if i can do it
+        we are trying to evaluate expressions in left-right order
+        we can apply the operation on the last two operands
+        and then store the result within the stack
+        repeat the process
+
+        iterate through tokens and append
+        we have to use a stack to keep track of the operands
+        when we reach an operation
+        we perform that 
+        and append the result
+        '''
+        stack = []
+
+        for token in tokens:
+            if token not in {"+", "-", "/", "*"}:
+                stack.append(int(token))
+            elif len(stack) >= 2:
+                total = 0
+                second, first = stack.pop(), stack.pop()
+                if token == "+":
+                    total = first + second
+                elif token == "-":
+                    total = first - second
+                elif token == "/":
+                    total = int(first / second)
+                else:
+                    total = first * second
+                stack.append(total)
+                print(stack)
+
+        return stack[-1]
+
+
+        
