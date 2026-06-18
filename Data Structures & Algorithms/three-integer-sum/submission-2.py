5e1e1a4c1ae901c7e54146dfd292bf9f48693bc0
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2026-06-18 13:11:45 -0500

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        '''
+        we are trying to return 3 numbers at different indices
+        that add up to 0. similar to two sum but adds a constraint
+        
+        i don't think we need a hashmap to store indices
+        we can loop through the list
+        and then use two pointers to find solution
+        '''
+        nums.sort()
+        res = []
+        seen = set()
+
+        for i in range(len(nums)):
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    indices = (nums[i], nums[left], nums[right])
+                    if indices not in seen:
+                        res.append(indices)
+                        seen.add(indices)
+                    left += 1
+                    right -= 1
+
+        return res
+                
