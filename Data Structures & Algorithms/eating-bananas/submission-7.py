eaceae1f889751c9898ee555bd0708148628a767
Kayz-xx <83003158+Kayz-xx@users.noreply.github.com>
2026-06-30 18:37:22 -0500

    Add: eating-bananas - submission-7

---
+++ Data Structures & Algorithms/eating-bananas/submission-7.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        '''
+        what kind of a dumb problem is this,
+        we're trying to return the minimum rate k
+        such that we can eat all bananas with h hours,
+        so it is a greedy problem
+
+        we can first sort the array to find the largest
+        pile, we chose that as our initial rate?
+        then we run binary search to find the point of
+        optimality, where we equal or less than h. 
+        '''
+        left, right = 1, max(piles)
+        rate = right
+
+        while left <= right:
+            mid = (left + right) // 2
+            hours = sum(math.ceil(pile / mid) for pile in piles)
+            if hours <= h:
+                right = mid - 1
+                rate = mid
+            elif hours > h:
+                left = mid + 1
+
+        return rate 
+
